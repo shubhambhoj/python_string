f4b9579aa6215555abe5a7568d416161a2a76d1c
shubhambhoj <shubhambhoj75@gmail.com>
2022-09-10 13:13:49 +0530

    add string

---
+++ string.py
@@ -0,0 +1,46 @@
+# CREATING STRING
+
+# SINGLE QUOTE
+
+str1 = 'hii,shubham welcome to home'         
+print(str1)
+
+#DOUBLE QUOTE
+
+str2="string represents group of characters"
+print(str2)
+
+# TRIPLE SINGLE QUOTE
+
+str3 = '''this is useful to create strings
+which span into 
+several lines'''
+print(str3)
+
+# TRIPLE  DOUBLE QUOTE 
+
+str4 = """Hello,Guys
+wel come to geeky shows """
+print(str4)
+
+# DOUBLE QUOTE INSIDE SINGLE QUOTE
+
+str5 = 'hii,"Ram" how are you ?'
+print(str5)
+
+# SINGLE QUOTE INSIDE DOUBLE QUOTE
+
+str6 = "hello,'shubham' where were you ?"
+print(str6)
+
+# USING ESCAPE CHARACTERS(\n,\t,\',\",etc)
+
+str7 = "hii,Ram \nhow are you ?"
+print(str7)
+
+# RAW STRING
+# It nullify the effect of Escape characters
+
+str8 = r"hello shubham \t where were you?"
+print(str8)
+
